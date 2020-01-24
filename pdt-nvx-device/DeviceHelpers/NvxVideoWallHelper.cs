1ba6e90da54d4071a2796d50f187d8780667cb9c
Andrew Welker <awelker@pepperdash.com>
2020-01-24 12:19:48 -0700

    NVX-3 removed streamchange event subscription

--- pdt-nvx-device/DeviceHelpers/NvxVideoWallHelper.cs
+++ pdt-nvx-device/DeviceHelpers/NvxVideoWallHelper.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using Crestron.SimplSharp.Reflection;
 using Crestron.SimplSharpPro;
-using Crestron.SimplSharpPro.DeviceSupport;
 using Crestron.SimplSharpPro.DM;
 using Crestron.SimplSharpPro.DM.Streaming;
 using PepperDash.Essentials.Core.Config;
 using EssentialsExtensions.Attributes;
-using Feedback = PepperDash.Essentials.Core.Feedback;
+using PepperDash.Essentials.Core;
 
 namespace NvxEpi.DeviceHelpers
 {
@@ -32,21 +31,10 @@ namespace NvxEpi.DeviceHelpers
             _key = config.Key;
             Feedback = FeedbackFactory.GetFeedback(() => VideoWallMode);
 
-            _device.HdmiOut.StreamChange += HdmiOutOnStreamChange;
             _device.BaseEvent += DeviceOnBaseEvent;
         }
 
         private void DeviceOnBaseEvent(GenericBase device, BaseEventArgs args)
-        {
-            switch (args.EventId)
-            {
-                case DMOutputEventIds.VideoWallModeFeedbackEventId:
-                    Feedback.FireUpdate();
-                    break;
-            }
-        }
-
-        private void HdmiOutOnStreamChange(Stream stream, StreamEventArgs args)
         {
             switch (args.EventId)
             {
