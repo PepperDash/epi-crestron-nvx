245fbd830d4f7d484477d5a6eabf1bb8a38f7ebe
Nick Genovese <ngenovese@pepperdash.com>
2020-11-10 15:33:26 -0500

    made hdmi inputs routing input advanced

--- src/NvxEpi/Services/InputPorts/HdmiInput1Port.cs
+++ src/NvxEpi/Services/InputPorts/HdmiInput1Port.cs
@@ -12,12 +12,28 @@ namespace NvxEpi.Services.InputPorts
             if (device.Hardware.HdmiIn == null || device.Hardware.HdmiIn[1] == null)
                 throw new NotSupportedException("hdmi 1");
 
-            var port = new RoutingInputPort(
-                DeviceInputEnum.Hdmi1.Name, 
-                eRoutingSignalType.AudioVideo, 
-                eRoutingPortConnectionType.Hdmi, 
-                DeviceInputEnum.Hdmi1, 
-                device);
+            var hdmi = device.Hardware.HdmiIn[1];
+            var port = new RoutingInputPortWithVideoStatuses(
+                DeviceInputEnum.Hdmi1.Name,
+                eRoutingSignalType.AudioVideo,
+                eRoutingPortConnectionType.Hdmi,
+                DeviceInputEnum.Hdmi1,
+                device,
+                new VideoStatusFuncsWrapper
+                    {
+                        HasVideoStatusFunc = () => true,
+                        HdcpActiveFeedbackFunc = () => hdmi.HdcpSupportOnFeedback.BoolValue,
+                        HdcpStateFeedbackFunc = () => hdmi.HdcpCapabilityFeedback.ToString(),
+                        VideoResolutionFeedbackFunc =
+                            () =>
+                                string.Format("{0}x{1}",
+                                    hdmi.VideoAttributes.HorizontalResolutionFeedback.UShortValue,
+                                    hdmi.VideoAttributes.VerticalResolutionFeedback.UShortValue),
+                        VideoSyncFeedbackFunc = () => hdmi.SyncDetectedFeedback.BoolValue
+                    });
+
+            hdmi.StreamChange += (stream, args) => port.VideoStatus.FireAll();
+            hdmi.VideoAttributes.AttributeChange += (sender, args) => port.VideoStatus.FireAll();
 
             device.InputPorts.Add(port);
         }
--- src/NvxEpi/Services/InputPorts/HdmiInput2Port.cs
+++ src/NvxEpi/Services/InputPorts/HdmiInput2Port.cs
@@ -12,12 +12,28 @@ namespace NvxEpi.Services.InputPorts
             if (device.Hardware.HdmiIn == null || device.Hardware.HdmiIn[2] == null)
                 throw new NotSupportedException("hdmi 2");
 
-            var port = new RoutingInputPort(
+            var hdmi = device.Hardware.HdmiIn[2];
+            var port = new RoutingInputPortWithVideoStatuses(
                 DeviceInputEnum.Hdmi2.Name,
                 eRoutingSignalType.AudioVideo,
                 eRoutingPortConnectionType.Hdmi,
                 DeviceInputEnum.Hdmi2,
-                device);
+                device,
+                new VideoStatusFuncsWrapper
+                    {
+                        HasVideoStatusFunc = () => true,
+                        HdcpActiveFeedbackFunc = () => hdmi.HdcpSupportOnFeedback.BoolValue,
+                        HdcpStateFeedbackFunc = () => hdmi.HdcpCapabilityFeedback.ToString(),
+                        VideoResolutionFeedbackFunc =
+                            () =>
+                                string.Format("{0}x{1}",
+                                    hdmi.VideoAttributes.HorizontalResolutionFeedback.UShortValue,
+                                    hdmi.VideoAttributes.VerticalResolutionFeedback.UShortValue),
+                        VideoSyncFeedbackFunc = () => hdmi.SyncDetectedFeedback.BoolValue
+                    });
+
+            hdmi.StreamChange += (stream, args) => port.VideoStatus.FireAll();
+            hdmi.VideoAttributes.AttributeChange += (sender, args) => port.VideoStatus.FireAll();
 
             device.InputPorts.Add(port);
         }
