42a8514b8a86ef6bdddc30e05487b211748f7613
Nick Genovese <ngenovese@pepperdash.com>
2021-10-11 12:11:58 -0400

    Revert "Revert "Feature/usb routing""
    This reverts commit 258ca340d9c20e91d3d68b7f071a0f28c57c4d61.

--- src/NvxEpi/Devices/Nvx35x.cs
+++ src/NvxEpi/Devices/Nvx35x.cs
@@ -7,9 +7,12 @@ using Crestron.SimplSharpPro.DM.Streaming;
 using NvxEpi.Abstractions;
 using NvxEpi.Abstractions.HdmiInput;
 using NvxEpi.Abstractions.HdmiOutput;
+using NvxEpi.Abstractions.Stream;
 using NvxEpi.Abstractions.Usb;
+using NvxEpi.Features.Config;
 using NvxEpi.Features.Hdmi.Input;
 using NvxEpi.Features.Hdmi.Output;
+using NvxEpi.Features.Streams.Usb;
 using NvxEpi.Services.Bridge;
 using NvxEpi.Services.InputPorts;
 using NvxEpi.Services.InputSwitching;
@@ -24,20 +27,22 @@ namespace NvxEpi.Devices
         NvxBaseDevice, 
         IComPorts, 
         IIROutputPorts,
-        IUsbStream, 
+        IUsbStreamWithHardware, 
         IHdmiInput, 
         IVideowallMode, 
         IRouting, 
-        ICec, 
+        ICec,
         INvx35XDeviceWithHardware
     {
         private IHdmiInput _hdmiInput;
         private IVideowallMode _hdmiOutput;
-        private readonly IUsbStream _usbStream;
+        private IUsbStreamWithHardware _usbStream;
+        private readonly NvxDeviceProperties _config;
 
         public Nvx35X(DeviceConfig config, Func<DmNvxBaseClass> getHardware, bool isTransmitter)
             : base(config, getHardware, isTransmitter)
         {
+            _config = NvxDeviceProperties.FromDeviceConfig(config);
             AddPreActivationAction(AddRoutingPorts);
         }
 
@@ -48,11 +53,13 @@ namespace NvxEpi.Devices
                 throw new Exception("hardware built doesn't match");
 
             Hardware = hardware;
+            var result = base.CustomActivate();
 
+            _usbStream = UsbStream.GetUsbStream(this, _config.Usb);
             _hdmiInput = new HdmiInput2(this);
             _hdmiOutput = new VideowallModeOutput(this);
 
-            return base.CustomActivate();
+            return result;
         }
 
         public CrestronCollection<ComPort> ComPorts
@@ -96,9 +103,9 @@ namespace NvxEpi.Devices
             get { return _usbStream.IsRemote; }
         }
 
-        public StringFeedback UsbId
+        public ReadOnlyDictionary<uint, StringFeedback> UsbRemoteIds
         {
-            get { return _usbStream.UsbId; }
+            get { return _usbStream.UsbRemoteIds; }
         }
 
         public int NumberOfComPorts
@@ -178,6 +185,11 @@ namespace NvxEpi.Devices
             {
                 StreamInput.AddRoutingPort(this);
             }
+        }
+
+        public StringFeedback UsbLocalId
+        {
+            get { return _usbStream.UsbLocalId; }
         }
     }
 }
