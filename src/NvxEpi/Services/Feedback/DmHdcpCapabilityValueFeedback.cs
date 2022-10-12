741632324e4389fb150019c932429867b7a87693
Nick Genovese <ngenovese@pepperdash.com>
2022-10-12 14:19:43 -0400

    fix: potentially fixed no source sync detection when using an e760x

---
+++ src/NvxEpi/Services/Feedback/DmHdcpCapabilityValueFeedback.cs
@@ -0,0 +1,39 @@
+﻿using Crestron.SimplSharpPro.DM.Streaming;
+using PepperDash.Essentials.Core;
+
+namespace NvxEpi.Services.Feedback
+{
+    public class DmHdcpCapabilityValueFeedback
+    {
+        public static IntFeedback GetFeedback(DmNvxBaseClass device)
+        {
+            var dmDevice = device as DmNvxE760x;
+            if (dmDevice == null)
+                return new IntFeedback(() => 0);
+
+            var feedback = new IntFeedback(Hdmi1HdcpCapabilityValueFeedback.Key,
+                () => (int)device.DmIn.HdcpCapabilityFeedback);
+
+            device.DmIn.InputStreamChange += (stream, args) => feedback.FireUpdate();
+            return feedback;
+        }
+    }
+
+    public class DmSyncDetectedFeedback
+    {
+        public const string Key = "Hdmi1SyncDetected";
+
+        public static BoolFeedback GetFeedback(DmNvxBaseClass device)
+        {
+            var dmDevice = device as DmNvxE760x;
+            if (dmDevice == null)
+                return new BoolFeedback(() => false);
+
+            var feedback = new BoolFeedback(Key,
+                () => device.DmIn.SyncDetectedFeedback.BoolValue);
+
+            device.DmIn.InputStreamChange += (stream, args) => feedback.FireUpdate();
+            return feedback;
+        }
+    }
+}
