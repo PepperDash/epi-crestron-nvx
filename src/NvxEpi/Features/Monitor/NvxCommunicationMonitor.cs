b6dd2bf5b88fb5e77ee6901b0b38a8e87a593475
Nick Genovese <ngenovese@pepperdash.com>
2022-10-19 13:41:11 -0400

    fix: added logging to the com monitor class for debugging

--- src/NvxEpi/Features/Monitor/NvxCommunicationMonitor.cs
+++ src/NvxEpi/Features/Monitor/NvxCommunicationMonitor.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Crestron.SimplSharp;
-using Crestron.SimplSharpPro;
+﻿using Crestron.SimplSharpPro;
 using PepperDash.Core;
 using PepperDash.Essentials.Core;
 
@@ -11,28 +6,35 @@ namespace NvxEpi.Features.Monitor
 {
     public class NvxCommunicationMonitor : StatusMonitorBase
     {
+        private readonly IKeyed _parent;
         public readonly GenericBase Hardware;
 
         public NvxCommunicationMonitor(IKeyed parent, long warningTime, long errorTime, GenericBase hardware) : base(parent, warningTime, errorTime)
         {
+            _parent = parent;
             Hardware = hardware;
             hardware.OnlineStatusChange += (device, args) => CheckIfDeviceIsOnlineAndUpdate();
+            StatusChange += (sender, args) => Debug.Console(1, parent, Debug.ErrorLogLevel.Notice, "Essentials Online Status:{0}", Status.ToString());
+            Status = MonitorStatus.StatusUnknown;
         }
 
         public override void Start()
         {
+            Debug.Console(1, _parent, Debug.ErrorLogLevel.Notice, "Starting Monitor...");
+            Status = MonitorStatus.StatusUnknown;
             CheckIfDeviceIsOnlineAndUpdate();
-            IsOnlineFeedback.FireUpdate();
         }
 
         public override void Stop()
         {
+            Debug.Console(1, _parent, Debug.ErrorLogLevel.Notice, "Stopping Monitor...");
             StopErrorTimers();
             Status = MonitorStatus.StatusUnknown;
         }
 
         public void CheckIfDeviceIsOnlineAndUpdate()
         {
+            Debug.Console(1, _parent, Debug.ErrorLogLevel.Notice, "Checking if device is online:{0}", Hardware.IsOnline);
             if (Hardware.IsOnline)
             {
                 Status = MonitorStatus.IsOk;
