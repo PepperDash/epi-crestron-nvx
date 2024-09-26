42d3c390861d29efa51fc134f0d577d7ebf7658d
Neil Dorin <ndorin@pepperdash.com>
2024-09-26 11:54:45 -0600

    feat: adds ICommunicationMonitor to NvxXioDirector

--- src/NvxEpi/Devices/NvxXioDirector.cs
+++ src/NvxEpi/Devices/NvxXioDirector.cs
@@ -1,20 +1,32 @@
 ﻿using System;
 using Crestron.SimplSharpPro.DM.Streaming;
 using NvxEpi.Abstractions.Hardware;
+using NvxEpi.Features.Monitor;
 using PepperDash.Essentials.Core;
 
 namespace NvxEpi.Devices;
 
-public class NvxXioDirector : EssentialsDevice, INvxDirector, IOnline
+public class NvxXioDirector : EssentialsDevice, INvxDirector, IOnline, ICommunicationMonitor
 {
     private readonly BoolFeedback _isOnline;
     private readonly DmXioDirectorBase _hardware;
+
+    public StatusMonitorBase CommunicationMonitor { get; private set; }
 
     public NvxXioDirector(string key, string name, DmXioDirectorBase hardware) : base(key, name)
     {
         _hardware = hardware ?? throw new ArgumentNullException("hardware");
         _isOnline = new BoolFeedback("BuildFeedbacks", () => _hardware.IsOnline);
         _hardware.OnlineStatusChange += (device, args) => _isOnline.FireUpdate();
+
+        AddPreActivationAction(() => CommunicationMonitor = new NvxCommunicationMonitor(this, 10000, 30000, _hardware));
+    }
+
+    public override bool CustomActivate()
+    {
+        CommunicationMonitor.Start();
+
+        return base.CustomActivate();
     }
 
     public BoolFeedback IsOnline
