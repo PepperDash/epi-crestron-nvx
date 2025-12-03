b4d8af9c7bf6e7ed44895f9dd17763c551ca6c87
Andrew Welker <awelker@pepperdash.com>
2025-12-03 12:36:15 -0600

    fix: update Essentials reference

--- src/NvxEpi/Factories/NvxBaseDeviceFactory.cs
+++ src/NvxEpi/Factories/NvxBaseDeviceFactory.cs
@@ -10,7 +10,7 @@ namespace NvxEpi.Factories;
 
 public abstract class NvxBaseDeviceFactory<T> : EssentialsPluginDeviceFactory<T> where T : EssentialsDevice
 {
-    public const string MinumumEssentialsVersion = "2.12.4";
+    public const string MinumumEssentialsVersion = "2.25.0";
 
     static NvxBaseDeviceFactory()
     {
