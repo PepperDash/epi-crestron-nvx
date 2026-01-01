ae2e075e1d177b0940ebec2fefe96ed8bfa2f22b
Nick Genovese <ngenovese@pepperdash.com>
2025-12-31 18:23:16 -0600

    fix: add sync status to feedback list

--- src/NvxEpi/Devices/NvxMockDevice.cs
+++ src/NvxEpi/Devices/NvxMockDevice.cs
@@ -158,6 +158,7 @@ public class NvxMockDevice
                 SecondaryAudioStreamStatus,
                 IsOnline,
                 StreamUrl,
+                SyncDetected,
             }
         );
 
