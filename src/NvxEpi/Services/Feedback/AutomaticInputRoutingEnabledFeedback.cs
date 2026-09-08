90838b5a150f9f383eb62a659c7ae34420b7508d
jkdevito <jdevito@pepperdash.com>
2026-09-08 14:17:08 -0500

    fix: guard NullSig BoolValue access in AutomaticInputRoutingEnabledFeedback

--- src/NvxEpi/Services/Feedback/AutomaticInputRoutingEnabledFeedback.cs
+++ src/NvxEpi/Services/Feedback/AutomaticInputRoutingEnabledFeedback.cs
@@ -1,3 +1,4 @@
+using System;
 using Crestron.SimplSharpPro.DM.Streaming;
 using PepperDash.Essentials.Core;
 
@@ -9,8 +10,20 @@ public class AutomaticInputRoutingEnabledFeedback
 
     public static BoolFeedback GetFeedback(DmNvxBaseClass device)
     {
-        var feedback = new BoolFeedback(Key,
-            () => device.Control.EnableAutomaticInputRoutingFeedback?.BoolValue ?? false);
+        var feedback = new BoolFeedback(Key, () =>
+        {
+            // Some NVX models/firmware don't populate this join - Crestron returns its
+            // internal NullSig sentinel (not a C# null) for it, so BoolValue throws rather
+            // than the reference being null. A null-conditional access can't catch that.
+            try
+            {
+                return device.Control.EnableAutomaticInputRoutingFeedback.BoolValue;
+            }
+            catch (InvalidOperationException)
+            {
+                return false;
+            }
+        });
 
         device.BaseEvent += (@base, args) => feedback.FireUpdate();
         return feedback;
